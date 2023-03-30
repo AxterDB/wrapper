3dd43b208f06b261ae6754da62dc00e3b78750eb
ItsNeil <68216747+ItsNeil17@users.noreply.github.com>
2023-03-31 02:47:59 +0800

    Add: Sphinx Extensions

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,25 @@ release = 'v0.1-alpha'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # "builder",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.duration",
+    "sphinxcontrib_trio",
+    # "details",
+    "exception_hierarchy",
+    "attributetable",
+    "resourcelinks",
+    "nitpick_file_ignorer",
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinxext.opengraph",
+    "sphinx_autodoc_typehints",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
